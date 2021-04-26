9194daf5da84d9feccf02fa07342f197777c35ba
gakusei <gakusei@iMac22.local>
2021-04-26 16:03:28 +0900

    label textの変更

--- 2021_IP3S_IW13A_12/ViewController.swift
+++ 2021_IP3S_IW13A_12/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "テスト"
     }
 
-    @IBOutlet weak var label: UILabel!
     
 
 }
