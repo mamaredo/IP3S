2277774423a5ff64513ac7b1bd630fd088888972
gakusei <gakusei@iMac02.local>
2021-07-05 16:07:47 +0900

    todoアプリケーションのプロジェクトを作成

---
+++ 2021_IP3S_IW13A_02_todo/2021_IP3S_IW13A_02_todo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  2021_IP3S_IW13A_02_todo
+//
+//  Created by gakusei on 2021/07/05.
+//  Copyright © 2021年 halnagoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 2021_IP3S_IW13A_02_todo/2021_IP3S_IW13A_02_todoTests/_021_IP3S_IW13A_02_todoTests.swift
@@ -0,0 +1,34 @@
+//
+//  _021_IP3S_IW13A_02_todoTests.swift
+//  2021_IP3S_IW13A_02_todoTests
+//
+//  Created by gakusei on 2021/07/05.
+//  Copyright © 2021年 halnagoya. All rights reserved.
+//
+
+import XCTest
+@testable import _021_IP3S_IW13A_02_todo
+
+class _021_IP3S_IW13A_02_todoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
